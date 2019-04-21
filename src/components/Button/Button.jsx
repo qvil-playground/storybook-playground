db9153eab3d529b449e718536f4c241d26f261d7
Taesu Hyeon <qvil1127@gmail.com>
2019-04-22 00:51:46 +0900

    Add Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 1rem;
+  outline: none;
+  border-radius: 5%;
+  background: papayawhip;
+  font-size: 1rem;
+`;
+
+const Button = ({ children, ...others }) => {
+  return <StyledButton {...others}>{children}</StyledButton>;
+};
+
+export default Button;
